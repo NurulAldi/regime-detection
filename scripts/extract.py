a5abfeefb678b31e597023db2adf765c786eb9ab
NurulAldi <aldiprm48@gmail.com>
2026-03-14 15:50:27 +0700

    finished

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 
-data = yf.download(["^GSPC", "^VIX"], start="2010-01-01", end="2024-01-01")['Close']
+data = yf.download(["^GSPC", "^VIX"], start="2010-01-01", end="2026-01-01")['Close']
 
 data.to_csv("data/data_market.csv")
 print("File downloaded successfully")
